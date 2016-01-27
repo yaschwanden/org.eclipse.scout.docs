e1933f8a8098b34da0819a9da2c787cca08963ef
Judtih Gull <jgu@bsiag.com>
2016-01-27 12:18:17 +0100

    add empty email field test

---
+++ code/contacts/org.eclipse.scout.contacts.client/src/test/java/org/eclipse/scout/contacts/client/EmailFieldTest.java
@@ -0,0 +1,10 @@
+package org.eclipse.scout.contacts.client;
+
+import org.eclipse.scout.contacts.client.template.AbstractEmailField;
+
+/**
+ * Test for {@link AbstractEmailField}
+ */
+public class EmailFieldTest {
+
+}
