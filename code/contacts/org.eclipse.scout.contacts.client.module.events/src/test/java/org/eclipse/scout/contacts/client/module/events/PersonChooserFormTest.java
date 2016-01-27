deaf5e3424ddbae763ac04f5f0de9cb689c6343c
Judtih Gull <jgu@bsiag.com>
2016-01-27 12:27:27 +0100

    prepare empty PersonChooserFormTest

---
+++ code/contacts/org.eclipse.scout.contacts.client.module.events/src/test/java/org/eclipse/scout/contacts/client/module/events/PersonChooserFormTest.java
@@ -0,0 +1,10 @@
+package org.eclipse.scout.contacts.client.module.events;
+
+import org.eclipse.scout.contacts.client.module.events.person.PersonChooserForm;
+
+/**
+ * Test for {@link PersonChooserForm}
+ */
+public class PersonChooserFormTest {
+
+}
