a539c509f4566322909bcf7ed1f5b0c735a9b3b1
Judtih Gull <jgu@bsiag.com>
2016-01-29 14:31:10 +0100

    Add testing exercises

---
+++ code/contacts/org.eclipse.scout.contacts.server/src/test/java/org/eclipse/scout/contacts/server/OrganizationServiceTest.java
@@ -0,0 +1,45 @@
+package org.eclipse.scout.contacts.server;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import org.eclipse.scout.contacts.server.organization.OrganizationService;
+import org.eclipse.scout.contacts.server.sql.DBSetupService;
+import org.eclipse.scout.contacts.server.sql.DerbySqlService;
+import org.eclipse.scout.contacts.shared.organization.OrganizationFormData;
+import org.eclipse.scout.rt.platform.BEANS;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * Tests for
+ * <h3>{@link OrganizationService}</h3>
+ */
+//TODO 7.1 Testing: Add testing annotations
+public class OrganizationServiceTest {
+
+  @BeforeClass
+  public static void setupDatabase() {
+    BEANS.get(DBSetupService.class).createOrganizationTable();
+  }
+
+  @AfterClass
+  public static void destroyDBConnections() {
+    BEANS.get(DerbySqlService.class).destroySqlConnectionPool();
+  }
+
+  @Test
+  public void testCreate() {
+    OrganizationFormData data = new OrganizationFormData();
+    data.getName().setValue("test");
+    OrganizationService svc = new OrganizationService();
+    OrganizationFormData created = svc.create(data);
+
+    assertEquals("test", created.getName().getValue());
+    assertNotNull(created.getOrganizationId());
+  }
+
+  //TODO 7.2 Testing: Add Tests for other OrganizationService methods
+
+}
