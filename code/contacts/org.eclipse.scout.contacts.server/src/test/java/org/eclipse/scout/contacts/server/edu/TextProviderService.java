85361cbc11221fcf7752b2ee7075e251900ba13d
Andreas Hoegger <Andreas.Hoegger@bsiag.com>
2016-12-05 14:57:44 +0100

    add an edu text provider service
    Change-Id: Ib3d036633baa3c1581362c7b531500076d0fafa1

---
+++ code/contacts/org.eclipse.scout.contacts.server/src/test/java/org/eclipse/scout/contacts/server/edu/TextProviderService.java
@@ -0,0 +1,8 @@
+package org.eclipse.scout.contacts.server.edu;
+
+import org.eclipse.scout.rt.platform.Order;
+import org.eclipse.scout.rt.shared.services.common.text.ScoutTextProviderService;
+
+@Order(0)
+public class TextProviderService extends ScoutTextProviderService {
+}
