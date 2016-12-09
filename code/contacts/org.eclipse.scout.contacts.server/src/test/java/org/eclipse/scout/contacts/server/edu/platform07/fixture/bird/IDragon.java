951b0d5dbb848f9220beeea695f5123313f5cb8d
Andreas Hoegger <Andreas.Hoegger@bsiag.com>
2016-12-09 11:21:28 +0100

    IgnoreBean annotation test
    Change-Id: Ic79d2c4cc44d2da09f2a4256d3c53a8963f7cde8

---
+++ code/contacts/org.eclipse.scout.contacts.server/src/test/java/org/eclipse/scout/contacts/server/edu/platform07/fixture/bird/IDragon.java
@@ -0,0 +1,13 @@
+package org.eclipse.scout.contacts.server.edu.platform07.fixture.bird;
+
+import org.eclipse.scout.rt.platform.IgnoreBean;
+
+/**
+ * <h3>{@link IDragon}</h3>
+ *
+ * @author aho
+ */
+@IgnoreBean
+public interface IDragon extends IBird {
+
+}
---
+++ code/contacts/org.eclipse.scout.contacts.server/src/test/java/org/eclipse/scout/contacts/server/edu/platform07/fixture/fish/Whale.java
@@ -0,0 +1,13 @@
+package org.eclipse.scout.contacts.server.edu.platform07.fixture.fish;
+
+import org.eclipse.scout.rt.platform.IgnoreBean;
+
+/**
+ * <h3>{@link Whale}</h3>
+ *
+ * @author aho
+ */
+@IgnoreBean
+public class Whale implements IMammal {
+
+}
