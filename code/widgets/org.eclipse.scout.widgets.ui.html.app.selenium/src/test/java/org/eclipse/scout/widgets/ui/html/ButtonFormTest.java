2a0e6fee34070ae22fd2cc21d75727feee8cb085
André Wegmüller <Andre.Wegmueller@bsi-software.com>
2016-12-13 10:05:26 +0100

    Added solution for selenium exercise

--- code/widgets/org.eclipse.scout.widgets.ui.html.app.selenium/src/test/java/org/eclipse/scout/widgets/ui/html/ButtonFormTest.java
+++ code/widgets/org.eclipse.scout.widgets.ui.html.app.selenium/src/test/java/org/eclipse/scout/widgets/ui/html/ButtonFormTest.java
@@ -2,11 +2,16 @@ package org.eclipse.scout.widgets.ui.html;
 
 import org.eclipse.scout.rt.ui.html.selenium.junit.AbstractSeleniumTest;
 import org.eclipse.scout.rt.ui.html.selenium.junit.SeleniumAssert;
+import org.eclipse.scout.rt.ui.html.selenium.util.SeleniumUtil;
 import org.eclipse.scout.widgets.client.ui.forms.ButtonForm.MainBox.ConfigurationBox.ButtonFieldButton;
 import org.eclipse.scout.widgets.client.ui.forms.ButtonForm.MainBox.ConfigurationBox.GetValueField;
+import org.eclipse.scout.widgets.client.ui.forms.ButtonForm.MainBox.ConfigurationBox.LabelField;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 /**
  * Selenium test for ButtonForm.
@@ -24,6 +29,16 @@ public class ButtonFormTest extends AbstractSeleniumTest {
     WebElement getValueField = findInputField(GetValueField.class);
     toggleButton.click();
     SeleniumAssert.assertInputFieldValue(this, getValueField, "true");
+  }
+
+  @Test
+  public void testButtonLabelTextChanges() {
+    WebElement labelInput = waitUntilInputFieldClickable(LabelField.class);
+    SeleniumUtil.sendKeysDelayed(labelInput, "Scout");
+    labelInput.sendKeys(Keys.TAB);
+    WebElement toggleButton = findElement(ButtonFieldButton.class);
+    WebElement spanInButton = toggleButton.findElement(By.tagName("span"));
+    waitUntil(ExpectedConditions.textToBePresentInElement(spanInButton, "Scout"));
   }
 
 }
