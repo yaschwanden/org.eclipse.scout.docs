ff28098d8e12d322b4004ddf567af8aebf0e5d02
Andi Bur <andi.bur@gmail.com>
2016-12-14 17:42:42 +0100

    Exercises Extensibility 2.0

---
+++ code/contacts/org.eclipse.scout.contacts.events.client/src/main/java/org/eclipse/scout/contacts/events/client/person/edu/EduPersonClientDomain.java
@@ -0,0 +1,39 @@
+/*******************************************************************************
+ * Copyright (c) 2016 BSI Business Systems Integration AG.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Distribution License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/org/documents/edl-v10.html
+ *
+ * Contributors:
+ *     BSI Business Systems Integration AG - initial API and implementation
+ ******************************************************************************/
+package org.eclipse.scout.contacts.events.client.person.edu;
+
+import org.eclipse.scout.rt.platform.BEANS;
+import org.eclipse.scout.rt.platform.IPlatform.State;
+import org.eclipse.scout.rt.platform.IPlatformListener;
+import org.eclipse.scout.rt.platform.PlatformEvent;
+import org.eclipse.scout.rt.shared.extension.IExtensionRegistry;
+
+/**
+ * Mock-up of person client domain.
+ */
+public class EduPersonClientDomain implements IPlatformListener {
+
+  @Override
+  public void stateChanged(PlatformEvent event) {
+    if (event.getState() == State.BeanManagerValid) {
+      IExtensionRegistry extensionRegistry = BEANS.get(IExtensionRegistry.class);
+      initializeExtensions(extensionRegistry);
+    }
+  }
+
+  protected void initializeExtensions(IExtensionRegistry registry) {
+    // model extensions
+    // TODO Extensibility 1.1: register form extension
+
+    // move field
+    // TODO Extensibility 2: register move
+  }
+}
---
+++ code/contacts/org.eclipse.scout.contacts.events.client/src/main/java/org/eclipse/scout/contacts/events/client/person/edu/EduPersonFormExtension.java
@@ -0,0 +1,22 @@
+/*******************************************************************************
+ * Copyright (c) 2016 BSI Business Systems Integration AG.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Distribution License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/org/documents/edl-v10.html
+ *
+ * Contributors:
+ *     BSI Business Systems Integration AG - initial API and implementation
+ ******************************************************************************/
+package org.eclipse.scout.contacts.events.client.person.edu;
+
+/*
+ * TODO Extensibility 1.1: uncomment extended class declaration, fix compile errors and add field 
+ *                         user TEXTS.get("POBox"); for its label
+ *                         
+ * TODO Extensibility 1.2: annotate class so that an extension data is created
+ * 
+ * TODO Extensibility 3: set DateOfBirthField invisible 
+ */
+public class EduPersonFormExtension /* extends AbstractFormExtension<PersonForm> */{
+}
