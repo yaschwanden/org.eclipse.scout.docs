268bfbee5063b6dbce676a9fac2c9e3541a12a8d
Andreas Hoegger <Andreas.Hoegger@bsiag.com>
2017-12-06 18:25:58 +0100

    Added MenuBarForm to the widgets app.
    Change-Id: I871cee6a2141ff01297a6b1b8abe01a0e8099944

--- code/widgets/org.eclipse.scout.jswidgets.ui.html/src/main/js/jswidgets-module.js
+++ code/widgets/org.eclipse.scout.jswidgets.ui.html/src/main/js/jswidgets-module.js
@@ -9,6 +9,7 @@
 (function(jswidgets, scout, $, undefined) {
   __include("jswidgets/App.js");
   __include("jswidgets/accordion/AccordionForm.js");
+  __include("jswidgets/action/ActionPropertiesBox.js");
   __include("jswidgets/carousel/CarouselForm.js");
   __include("jswidgets/common/IconIdLookupCall.js");
   __include("jswidgets/form/DisplayHintLookupCall.js");
@@ -31,6 +32,8 @@
   __include("jswidgets/groupbox/GroupBoxForm.js");
   __include("jswidgets/groupbox/GroupBoxFieldsLookupCall.js");
   __include("jswidgets/groupbox/GroupBoxPropertiesBox.js");
+  __include("jswidgets/menubar/MenuBarForm.js");
+  __include("jswidgets/menubar/MenuItemLookupCall.js");
   __include("jswidgets/numberfield/NumberFieldForm.js");
   __include("jswidgets/logicalgrid/GridDataBox.js");
   __include("jswidgets/logicalgrid/LogicalGridForm.js");
