1e17ae1da3f740310597311cb0abcd2118513d1b
Andreas Hoegger <Andreas.Hoegger@bsiag.com>
2019-03-25 14:00:44 +0100

    Extensibility exercises.
    Change-Id: I8bce29530290c496ba4d21514ec1024ecce893b2

---
+++ code/contacts/org.eclipse.scout.contacts.events.server/src/main/java/org/eclipse/scout/contacts/events/server/person/edu/EduPersonService.java
@@ -0,0 +1,28 @@
+/*******************************************************************************
+ * Copyright (c) 2016 BSI Business Systems Integration AG.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Distribution License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/org/documents/edl-v10.html
+ *
+ * Contributors:
+ *     BSI Business Systems Integration AG - initial API and implementation
+ ******************************************************************************/
+package org.eclipse.scout.contacts.events.server.person.edu;
+
+import org.eclipse.scout.contacts.events.server.person.PersonServiceExtension;
+import org.eclipse.scout.contacts.shared.person.PersonFormData;
+import org.eclipse.scout.rt.platform.Replace;
+
+/**
+ * Person service extension that logs P.O. Box value
+ */
+@Replace
+public class EduPersonService extends PersonServiceExtension {
+
+  @Override
+  public PersonFormData store(PersonFormData formData) {
+    // TODO Extensibility 1.3: Log contents of P.O. Box field
+    return super.store(formData);
+  }
+}
